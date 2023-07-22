5ee88242a427a9d4b752e3d54ee3765fd420ed78
devendra-alt <devendramulewa171@gmail.com>
2023-07-22 09:33:05 +0530

    Add base components for todoApp

---
+++ src/components/Header.jsx
@@ -0,0 +1,8 @@
+const Header = () => {
+  return (
+    <header>
+      <p>Items will persist in the browser local storage</p>
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/ToDo.jsx
@@ -0,0 +1,10 @@
+import Header from '@/components/Header';
+import TodosLogic from './ToDoLogic';
+const ToDo = () => (
+  <>
+    <Header />
+    <TodosLogic />
+  </>
+);
+
+export default ToDo;
