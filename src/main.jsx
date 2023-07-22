bae7245ca5d8943e25ba447e8600386d8369e847
devendra-alt <devendramulewa171@gmail.com>
2023-07-22 09:33:41 +0530

    Add default files for vite dev react app

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import ToDo from '@/components/ToDo';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ToDo />
+  </React.StrictMode>
+);
