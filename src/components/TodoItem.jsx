4e5b95ad5046c3671db3e925235aaafe895afc3e
devendra-alt <devendramulewa171@gmail.com>
2023-07-24 22:41:09 +0530

    Add edit ,delete,update for todoItem.jsx

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,44 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
-const TodoItem = ({ todoTask, handleCheckbox, delTodo }) => {
+const TodoItem = ({ todoTask, handleCheckbox, delTodo, setUpdate }) => {
+  const [editing, setEditing] = useState(false);
+  let vieWMode = {};
+  let editMode = {};
+  if (editing) {
+    vieWMode.display = 'none';
+  } else {
+    editMode.display = 'none';
+  }
+  const handleEditing = () => {
+    setEditing(true);
+  };
+  function handleUpdateDone(e) {
+    if (e.key === 'Enter') {
+      setEditing(false);
+    }
+  }
   return (
     <li>
+      <div style={vieWMode}>
+        <input
+          type="checkbox"
+          checked={todoTask.completed}
+          onChange={() => handleCheckbox(todoTask.id)}
+        />
+        {todoTask.title}
+        <button onClick={handleEditing}>Edit</button>
+        <button onClick={() => delTodo(todoTask.id)}>Delete</button>
+      </div>
       <input
-        type="checkbox"
-        checked={todoTask.completed}
-        onChange={() => handleCheckbox(todoTask.id)}
+        type="text"
+        value={todoTask.title}
+        style={editMode}
+        onChange={(e) => {
+          setUpdate(e.target.value, todoTask.id);
+        }}
+        onKeyDown={handleUpdateDone}
       />
-      {todoTask.title}
-      <button onClick={() => delTodo(todoTask.id)}>Delete</button>
     </li>
   );
 };
