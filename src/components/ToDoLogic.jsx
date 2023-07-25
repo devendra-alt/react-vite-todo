386aec72e39a10d617c63454db0e52c8602ab0ca
devendra-alt <devendramulewa171@gmail.com>
2023-07-25 13:23:26 +0530

    Formate eslint typos

--- src/components/ToDoLogic.jsx
+++ src/components/ToDoLogic.jsx
@@ -1,45 +1,50 @@
+import { v4 as uuidv4 } from 'uuid';
 import { useState, useEffect } from 'react';
 import InputTodo from './TodoInput';
 import TodoList from './TodoList';
-import { v4 as uuidv4 } from 'uuid';
 
 const TodosLogic = () => {
-  const [todos, setToDos] = useState(getInitialTodos());
-
   function getInitialTodos() {
     const temp = localStorage.getItem('todos');
     const savedTodos = JSON.parse(temp);
     return savedTodos || [];
   }
 
+  const [todos, setToDos] = useState(getInitialTodos());
+
   useEffect(() => {
     const temp = JSON.stringify(todos);
     localStorage.setItem('todos', temp);
   }, [todos]);
 
-  const addTodoItem = (title) => {
+  function addTodoItem(title) {
     const newTodo = {
       id: uuidv4(),
-      title: title,
+      title,
       completed: false,
     };
     setToDos([...todos, newTodo]);
-  };
+  }
 
-  const setUpdate = (updatedTitle, id) => {
-    setToDos((pre) =>
-      pre.map((el) => (el.id === id ? { ...el, title: updatedTitle } : el))
-    );
-  };
+  function setUpdate(updatedTitle, id) {
+    setToDos((pre) => {
+      pre.map((el) => (el.id === id ? { ...el, title: updatedTitle } : el));
+    });
+  }
 
   function delTodo(id) {
     setToDos([...todos.filter((todo) => todo.id !== id)]);
   }
 
   function handleCheckbox(id) {
-    setToDos((pre) =>
-      pre.map((el) => (el.id === id ? { ...el, completed: !el.completed } : el))
-    );
+    setToDos((pre) => {
+      pre.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      });
+    });
   }
 
   return (
