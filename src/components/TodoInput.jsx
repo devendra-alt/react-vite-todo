82f692e8c5082102e183feac15644372b1dd9d31
devendra-alt <devendramulewa171@gmail.com>
2023-07-25 13:21:51 +0530

    Add useCallback for handleChange method

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,14 +1,15 @@
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
 
 const InputTodo = ({ addTodoItem }) => {
   const [title, setTitle] = useState('');
   const [message, setMessage] = useState('');
 
-  const handleChange = (e) => {
+  const handleChange = useCallback((e) => {
     setMessage(false);
     e.preventDefault();
     setTitle(e.target.value);
-  };
+  });
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -22,12 +23,18 @@ const InputTodo = ({ addTodoItem }) => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={() => handleSubmit}>
       <input type="text" name="title" value={title} onChange={handleChange} />
-      <button className="todo-btn">Submit</button>
+      <button className="todo-btn" type="submit">
+        Submit
+      </button>
       <span className="error-msg">{message}</span>
     </form>
   );
+};
+
+InputTodo.propTypes = {
+  addTodoItem: PropTypes.func.isRequired,
 };
 
 export default InputTodo;
